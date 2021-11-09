f0b0a04c067681d8d42c7db863e3c4574d5bcab0
Mario Krenn <46604014+MarioKrenn6240@users.noreply.github.com>
2021-11-09 17:31:55 +0100

    Example of the GHZ state

---
+++ GHZ_example.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 13 12:39:33 2021
+
+@author: alejomonbar
+"""
+import Theseus
+
+Dimensions = 4 * [2] # Dimensions of all states
+State = [[0,0,0,0], [1,1,1,1]]
+Graph = Theseus.Graph(Dimensions)
+Fidelity = Graph.fidelity(State)
+sol, vars_ = Graph.topological_optimization(Fidelity)
+print("w_ab_00:", Graph.TargetEquation)
+print("Weights of the solution: ", vars_)
+Graph.plot(filename="GHZ_prior_optimization.png")
+Graph.plot(optimization=True,filename="GHZ_after_optimization.png")

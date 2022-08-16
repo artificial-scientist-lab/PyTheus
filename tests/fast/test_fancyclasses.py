a1812eee6e5e8f78af2863b6c8ecc7afdb13a80c
kafeelsyd <kafeel.syed@mpl.mpg.de>
2022-08-16 13:01:04 +0200

    test script for help_functions, not finalized yet

--- tests/fast/test_fancyclasses.py
+++ tests/fast/test_fancyclasses.py
@@ -36,8 +36,3 @@ class TestGraph(unittest.TestCase):
         self.assertEqual({
             ((0, 0), (1, 0)): [((0, 1, 0, 0),)], ((0, 1), (1, 1)): [((0, 1, 1, 1),)]
         }, graph.state_catalog)
-
-
-    #import pickle
-    #with open('graph.pck', 'rb') as fh:
-    #    graph = pickle.load(fh)
---
+++ tests/fast/test_saver.py
@@ -0,0 +1,8 @@
+import unittest
+
+from theseus.saver import saver
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, False)  # add assertion here
+

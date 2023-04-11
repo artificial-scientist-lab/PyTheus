7d09513fb7841d46e71380907e2f5664d8acc879
cruizgo <64470526+cruizgo@users.noreply.github.com>
2023-04-11 15:40:02 +0200

    NEW function `brutal_covers_heralding_sps`
    TODO: the fock related functions are still not using the fock state generators from `theseus.py`.

--- pytheus/lossfunctions.py
+++ pytheus/lossfunctions.py
@@ -9,7 +9,7 @@ import pytheus.theseus as th
 from collections import Counter
 import itertools
 import numpy as np
-from itertools import combinations
+from itertools import combinations, product
 from itertools import combinations_with_replacement
 from pytheus.custom_loss.assembly_index import assembly_index, top_n_assembly
 import logging
@@ -17,8 +17,46 @@ import logging
 log = logging.getLogger(__name__)
 
 
+def brutal_covers_heralding_sps(cnfg, graph):
+    # this function finds heralding edgecovers for a bipartite graph
+    in_verts = cnfg["single_emitters"]
+    out_verts = cnfg["out_nodes"] + cnfg["anc_detectors"]
+
+    avail_colors = th.edgeBleach(graph.edges)
+
+    sep_edges = []
+    for node in in_verts:
+        group = [edge for edge in list(avail_colors.keys()) if node in edge[:2]]
+        sep_edges.append(group)
+    tmp_raw_covers = list(product(*sep_edges))
+
+    raw_covers = []
+    for cover in tmp_raw_covers:
+        counter = Counter(list(itertools.chain(*cover)))
+        keep = True
+
+        if cnfg['number_resolving']:
+            for vert in cnfg["anc_detectors"]:
+                if counter[vert] != 1:
+                    keep = False
+        else:
+            for vert in cnfg["anc_detectors"]:
+                if counter[vert] == 0:
+                    keep = False
+        # if all conditions are met, use edgecover for norm
+        if keep:
+            raw_covers.append(cover)
+
+    painted_covers = []
+    for cover in raw_covers:
+        for coloring in itertools.product(*[avail_colors[edge] for edge in cover]):
+            color_cover = [edge + color for edge, color in zip(cover, coloring)]
+            painted_covers.append(sorted(color_cover))
+    return [[tuple(ed) for ed in graph] for graph in painted_covers]
+
+
 def brutal_covers(cnfg, graph):
-    # this functions is sometimes used instead of findEdgecovers, should be much slower in general, but faster at
+    # this function is sometimes used instead of findEdgecovers, should be much slower in general, but faster at
     # applying topological constraints atm
 
     non_output_verts = [vert for vert in cnfg["verts"] if vert not in cnfg["out_nodes"]]
@@ -55,13 +93,13 @@ def brutal_covers(cnfg, graph):
                 if counter[vert] == 0:
                     keep = False
 
-        # if in maximum order, we select for events where the correct total number of photons go into output paths
-        if len(cover) == max_edges:
-            sum = 0
-            for vert in cnfg["out_nodes"]:
-                sum += counter[vert]
-            if sum != len(cnfg["out_nodes"]):
-                keep = False
+        # # if in maximum order, we select for events where the correct total number of photons go into output paths
+        # if len(cover) == max_edges:
+        #     sum = 0
+        #     for vert in cnfg["out_nodes"]:
+        #         sum += counter[vert]
+        #     if sum != len(cnfg["out_nodes"]):
+        #         keep = False
 
         # if all conditions are met, use edgecover for norm
         if keep:
@@ -144,12 +182,15 @@ def count_rate(graph, target_state, cnfg):
     else:
         if not cnfg["brutal_covers"]:
             edgecovers = heralded_covers(cnfg, graph)
+        elif cnfg["bipartite"]:
+            edgecovers = brutal_covers_heralding_sps(cnfg, graph)
         else:
             edgecovers = brutal_covers(cnfg, graph)
         cat = th.stateCatalog(edgecovers)
         norm = th.writeNorm(cat, imaginary=cnfg["imaginary"])
     lambdaloss = "".join(["1-", target, "/(1+", norm, ")"])
     func, lossstring = th.buildLossString(lambdaloss, variables)
+    print('count rate done', flush=True)
     return func
 
 
@@ -170,23 +211,28 @@ def fidelity(graph, target_state, cnfg):
     else:
         if not cnfg["brutal_covers"]:
             edgecovers = heralded_covers(cnfg, graph)
+        elif cnfg["bipartite"]:
+            edgecovers = brutal_covers_heralding_sps(cnfg, graph)
         else:
             edgecovers = brutal_covers(cnfg, graph)
         cat = th.stateCatalog(edgecovers)
         norm = th.writeNorm(cat, imaginary=cnfg["imaginary"])
     lambdaloss = "".join(["1-", target, "/(0+", norm, ")"])
     func, lossstring = th.buildLossString(lambdaloss, variables)
+    print('fidelity done', flush=True)
     return func
 
 
 """
 Number states in Fock basis
 """
-def fock_fidelity(graph, target_state, num_anc, amplitudes, imaginary=False): #num_anc_pre
+
+
+def fock_fidelity(graph, target_state, num_anc, amplitudes, imaginary=False):  # num_anc_pre
 
     # original ket is in the form: [((0,m),(1,n),(2,k1),(3,k2)...]; 
     # here the m, n, k1, k2 are the number of particles instead of dimensions
-    
+
     # make the ket in the correct form 
     # [((0,0),(0,0)...,(1,0),(1,0),... (2,0),(2,0),...,(3,0),(3,0),...]
     # m times (0,0); n times (1,0); k1 times (2,0); k2 times (3,0), etc.
@@ -195,88 +241,83 @@ def fock_fidelity(graph, target_state, num_anc, amplitudes, imaginary=False): #n
     # accum = [sum([[(i,0)]*max(0,n) for i,n in k ],[]) for k in target_kets_temp]
     # for ii in accum:
     #     target_kets.append(tuple(ii))   
-        
-         
-    #all_verts= range(graph.num_nodes)#np.unique(list(itertools.chain(*th.edgeBleach(graph.edges).keys())))
-    #anc_each_num=[int(i) for i in num_anc_pre]
-    #state_each_num=[int(i) for i in target_state_str[0]] # all terms have the same photon number
-    
+
+    # all_verts= range(graph.num_nodes)#np.unique(list(itertools.chain(*th.edgeBleach(graph.edges).keys())))
+    # anc_each_num=[int(i) for i in num_anc_pre]
+    # state_each_num=[int(i) for i in target_state_str[0]] # all terms have the same photon number
+
     total_particle_num = len(target_state.kets[0])
-    #total_particle_num=sum(state_each_num)+num_anc #sum(anc_each_num)
-    
+    # total_particle_num=sum(state_each_num)+num_anc #sum(anc_each_num)
+
     # anc_position=all_verts[len(all_verts)-num_anc:]
     # anc_kets=[]
     # for jj in anc_position:
     #     anc_kets.append((jj,0))
-        
+
     anc_nodes = list(range(graph.num_nodes - num_anc, graph.num_nodes))
-    
-    
-    edgecover_target = list(itertools.combinations_with_replacement(graph.edges,int(total_particle_num/2)))
+
+    edgecover_target = list(itertools.combinations_with_replacement(graph.edges, int(total_particle_num / 2)))
     cat = th.stateCatalog(edgecover_target)
-    
-    if len(anc_nodes)>0:
+
+    if len(anc_nodes) > 0:
         for ket in list(cat.keys()):
             shopping = Counter(ket)
-            if all(shopping[(ii,0)]==1 for ii in anc_nodes):
+            if all(shopping[(ii, 0)] == 1 for ii in anc_nodes):
                 pass
             else:
                 del cat[ket]
-          
+
     # this cause a problem in the optimizer as it actually does not use len(useful_deges)
-    #useful_edges = sorted(set(sum(sum(cat.values(),[]), ())))
-    #variables = th.stringEdges(useful_edges)
-    
+    # useful_edges = sorted(set(sum(sum(cat.values(),[]), ())))
+    # variables = th.stringEdges(useful_edges)
+
     variables = th.stringEdges(graph.edges, imaginary=imaginary)
-    
-    target = th.targetEquation(target_state.kets,amplitudes=amplitudes,state_catalog=cat)
+
+    target = th.targetEquation(target_state.kets, amplitudes=amplitudes, state_catalog=cat)
     norm = th.writeNorm(cat)
 
-   # epsilon = 1e-10
+    # epsilon = 1e-10
     epsilon = 0
-    lambdaloss = f'1-({target})/({epsilon} + {norm})'   
-   #  lambdaloss="".join(["1-", target, "/(0+", norm, ")"])
-    
+    lambdaloss = f'1-({target})/({epsilon} + {norm})'
+    #  lambdaloss="".join(["1-", target, "/(0+", norm, ")"])
+
     func, lossstring = th.buildLossString(lambdaloss, variables)
     return func
 
-def fock_countrate(graph, target_state, num_anc, amplitudes, imaginary=False): #num_anc_pre
 
-  
-    
+def fock_countrate(graph, target_state, num_anc, amplitudes, imaginary=False):  # num_anc_pre
+
     total_particle_num = len(target_state.kets[0])
-        
+
     anc_nodes = list(range(graph.num_nodes - num_anc, graph.num_nodes))
-    
-    
-    edgecover_target = list(itertools.combinations_with_replacement(graph.edges,int(total_particle_num/2)))
+
+    edgecover_target = list(itertools.combinations_with_replacement(graph.edges, int(total_particle_num / 2)))
     cat = th.stateCatalog(edgecover_target)
-    
-    if len(anc_nodes)>0:
+
+    if len(anc_nodes) > 0:
         for ket in list(cat.keys()):
             shopping = Counter(ket)
-            if all(shopping[(ii,0)]==1 for ii in anc_nodes):
+            if all(shopping[(ii, 0)] == 1 for ii in anc_nodes):
                 pass
             else:
                 del cat[ket]
-          
+
     # this cause a problem in the optimizer as it actually does not use len(useful_deges)
-    #useful_edges = sorted(set(sum(sum(cat.values(),[]), ())))
-    #variables = th.stringEdges(useful_edges)
-    
+    # useful_edges = sorted(set(sum(sum(cat.values(),[]), ())))
+    # variables = th.stringEdges(useful_edges)
+
     variables = th.stringEdges(graph.edges, imaginary=imaginary)
-    
-    target = th.targetEquation(target_state.kets,amplitudes=amplitudes,state_catalog=cat)
+
+    target = th.targetEquation(target_state.kets, amplitudes=amplitudes, state_catalog=cat)
     norm = th.writeNorm(cat)
 
-   # epsilon = 1e-10
+    # epsilon = 1e-10
     epsilon = 1
-    lambdaloss = f'1-({target})/({epsilon} + {norm})'   
-   #  lambdaloss="".join(["1-", target, "/(1+", norm, ")"])
-    
+    lambdaloss = f'1-({target})/({epsilon} + {norm})'
+    #  lambdaloss="".join(["1-", target, "/(1+", norm, ")"])
+
     func, lossstring = th.buildLossString(lambdaloss, variables)
     return func
-
 
 
 def make_lossString_entanglement(graph, sys_dict: dict, imaginary=False,
@@ -338,6 +379,6 @@ loss_dic = {'ent': [make_lossString_entanglement],
             'fid': [fidelity, count_rate],
             'cr': [count_rate, fidelity],
             'lff': [loss_from_function],
-            'fockfid': [fock_fidelity,fock_countrate],
-            'fockcr': [fock_countrate,fock_fidelity]
-           }
+            'fockfid': [fock_fidelity, fock_countrate],
+            'fockcr': [fock_countrate, fock_fidelity]
+            }

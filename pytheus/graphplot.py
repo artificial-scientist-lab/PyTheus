b6aebeaba5ba8ec1b1af8fe87550bc4a10aabc79
Ozra Mahmoudi <50074433+mahmoudifar@users.noreply.github.com>
2023-04-17 14:56:32 +0330

    Update graphplot.py

--- pytheus/graphplot.py
+++ pytheus/graphplot.py
@@ -9,6 +9,18 @@ import json
 import os
 import pytheus.leiwand
 
+from matplotlib.patches import Rectangle, Wedge, Circle
+import matplotlib
+matplotlib.rcParams['figure.dpi']=300
+import itertools
+import random
+from matplotlib.markers import MarkerStyle
+from collections import Counter
+import matplotlib as mpl
+from collections.abc import Iterable
+from collections import Counter
+from ast import literal_eval
+
 
 def drawEdge(edge, verts, ind, mult, ax, scale_max=None, max_thickness=10,
              show_val=False, fs=15, markersize=25):
@@ -210,3 +222,97 @@ def plotFromFile(filename, number_nodes=True, outfile=""):
     # graph = Graph(sol_dict['graph'])
     # graphPlot(graph.graph, scaled_weights=True, number_nodes=number_nodes, filename=outfile)
     graphPlot(sol_dict['graph'], scaled_weights=True, number_nodes=number_nodes, filename=outfile)
+    
+##############################################################################################################################
+
+colors = ['dodgerblue', 'firebrick', 'limegreen', 'darkorange', 'purple', 'yellow', 'cyan']
+Paths = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k']
+
+#plot path and optical elements
+def Plot_BS(ax, X, Y, width, height, color):
+    ax.add_patch(Rectangle((X, Y), width, height,fc = 'lavender', \
+                           ec = 'navy', angle = 45, zorder =18))
+    d0 = np.sqrt(width**2+height**2)/4
+    ax.plot([X+d0, X-d0],[Y+d0, Y+3*d0 ],zorder = 20 , color = color)
+    ax.plot([X-d0, X+d0],[Y+d0, Y+3*d0 ],zorder = 20 , color = color)
+    ax.vlines(X, ymin = Y, ymax =Y+4*d0, colors ='navy',zorder = 19  )
+     
+def Plot_PBS(ax, X, Y, width,height, color1, color2 ):
+    ax.add_patch(Rectangle((X, Y), width, height,fc = 'thistle', \
+                           ec = 'indigo', angle = 45, zorder =18) )
+    d0 = np.sqrt(width**2+ height**2)/4
+    ax.plot([X+d0, X-d0],[Y+d0, Y+3*d0 ],zorder = 20 , color = color1)
+    ax.plot([X-d0, X],[Y+d0, Y+2*d0 ],zorder = 20 , color = color2)
+    ax.plot([X, X-d0],[Y+2*d0, Y+3*d0 ],zorder = 20 , color = color2,linestyle =':')
+    ax.vlines(X, ymin = Y, ymax =Y+4*d0, colors ='indigo',zorder = 19  )
+    
+def Plot_SPDC(ax, X, Y, width, height, color1, color2, W, wmax = 1 ):
+    ax.add_patch(Rectangle((X, Y), width/2, height, fc = color1, ec = 'none',alpha= abs(W)/wmax))
+    ax.add_patch(Rectangle((X+width/2,Y), width/2, height, fc = color2, ec ='none' , alpha= abs(W)/wmax))
+    ax.add_patch(Rectangle((X, Y),width, height, fc = \
+                           'none', ec ='black',zorder = 10 ))
+    d0 = width/10
+    d1 = Y+height
+    ax.vlines(X+d0, ymin = d1, ymax = d1+height, colors = color1)
+    ax.vlines(X+width-d0, ymin = d1, ymax = d1+height, colors = color2)  
+    
+def Plot_Absorber(ax , X , Y,  width, height) :  
+    ax.add_patch(Rectangle((X, Y), width, height,fc = 'k', ec = 'r',zorder=10, joinstyle= 'bevel', lw =2))
+    
+def Plot_Hline(ax , XMIN, XMAX, Y , color): 
+    ax.hlines(Y, xmin=XMIN, xmax=XMAX, colors=color, zorder = 9)
+    
+def Plot_Vline(ax , YMIN, YMAX, X , color ): 
+    ax.vlines(X, ymin=YMIN, ymax=YMAX, colors=color\
+              ,zorder = 8 )   
+
+def Plot_Connection_Line(ax,X,Y):
+    t=np.linspace(0,1,1000)
+    ax.plot(X[0]+(3*t**2-2*t**3)*(X[1]-X[0]),Y[0]+t*(Y[1]-Y[0]),
+           color='k')    
+
+def Plot_Detector(ax , X, Y, leng, step, radius ):
+    pos = Pos_Element(X,step,leng)
+    for ii in range(len(pos)):
+        ax.add_patch(Wedge((pos[ii], Y), radius,0, 180,fc = 'k', ec = 'k', zorder = 10))
+        ax.add_patch(Rectangle((pos[ii]-1.2*radius, Y-radius/2), 2.4*radius, radius/2, fc = 'k', ec = 'k', zorder = 12))
+        Plot_Connection_Line(ax, [pos[ii], pos[ii]-radius], [Y+radius,Y+2.5*radius] )
+
+def Plot_Crystal (X, Y, color, width, height, W, wmax =1): #for path identity
+    ax.add_patch(Rectangle((X, Y), width, height,fc = 'none', ec ='black' ,zorder=6))
+    row = len(color)
+    column = 2
+    y_crystal = Pos_Element(Y,height/row,row)
+    x_crystal = Pos_Element(X,width/column, column)
+    if len (y_crystal) == 1:
+        height1 = height
+    else:    
+        height1 = y_crystal[1]-y_crystal[0]
+    width1 = x_crystal[1]-x_crystal[0]
+    for y in range(len(y_crystal)):
+        posy = y_crystal[y]
+        ax.hlines(posy, xmin=X ,xmax=X+width, colors='k', zorder = 6)
+        for x in range(len(x_crystal)):
+            posx= x_crystal[x]
+            colors = color[y][x]
+            ax.add_patch(Rectangle((posx, posy), width1, height1,\
+                                   fc = colors, ec ='none' ,zorder=5, alpha =abs(W[y])/wmax ))
+            
+def Plot_Sorter(ax , X, Y, leng, step, width, height, color):
+    pos = Pos_Element(X,step,leng)
+    xmin = min(pos)
+    xmax = max(pos)
+    Plot_Hline(ax , xmin, xmax+width, Y+ height/10 , 'k')
+    Plot_Hline(ax , xmin, xmax+width, Y+9*height/10 , 'k')
+    Plot_Hline(ax , xmin, xmax+width, Y+height/2, 'k')
+    for p in range(len(pos)):
+        ax.add_patch(Circle((pos[p]+ width/2, Y+height/2), width/2,fc = color[p], ec = 'k', zorder = 15))
+        ax.add_patch(Rectangle((pos[p], Y), width, height,fc = 'lightgray', ec = 'k', zorder = 12))
+
+def Plot_Multi_Color_Line(X, Y, height, color, leng, radius):
+    step = height/float(leng)
+    y = Pos_Element(Y,step,leng)
+    Plot_Detector(ax , X, y[-1]+radius/2, 1, 1, radius )
+    loc = generate_N_grams (y, ngram = 2)
+    for pp in range(len(loc)):
+        Plot_Vline(ax , loc[pp][0],loc[pp][1], X, color[pp] ) 

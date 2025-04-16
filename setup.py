5ec2e45337d578f31fbfefaaefcb84dac05df044
Sören <69844028+soerenarlt@users.noreply.github.com>
2025-04-16 12:13:29 +0200

    update version number

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ url = 'https://github.com/artificial-scientist-lab/Pytheus'
 
 def get_version():
     content = open(Path(package_name) / '__init__.py').readlines()
-    return "1.2.18"
+    return "1.2.19"
 
 
 setup(
